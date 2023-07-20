27e180ae61dedaf32441dc95ce55ae63d6b3a734
siddy94 <pandit.siddharth94@gmail.com>
2023-07-20 17:00:40 +0530

    Update streamlit_app.py
    added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My parent\'s new Healthy Diner')
-streamlit.header('Healthy Breakfast Menu')
-streamlit.text('Omega3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie!!')
-streamlit.text('Hard-Boiled Free range Egg')
-streamlit.text('Avocado toast with Soymilk')
+streamlit.header('Breakfast Favourites')
+streamlit.text(' 🥣 Omega3 and Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free range Egg')
+streamlit.text(' 🥑🍞 Avocado Toast with Soymilk')

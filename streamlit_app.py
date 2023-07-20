2da7fa11b35aa88fe96b4ca23f2e4e616c818c37
siddy94 <pandit.siddharth94@gmail.com>
2023-07-20 16:58:23 +0530

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
+streamlit.title('My parent\'s new Healthy Diner')
+streamlit.header('Healthy Breakfast Menu')
+streamlit.text('Omega3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie!!')
+streamlit.text('Hard-Boiled Free range Egg')
+streamlit.text('Avocado toast with Soymilk')
